2c8c9f4c4a44062f773dce156b273cef7a07bc74
graemetlloyd <graemetlloyd@gmail.com>
2015-04-17 18:11:09 -0400

    Added roxygen information.

--- R/ThetaFromChordLength.R
+++ R/ThetaFromChordLength.R
@@ -1,3 +1,14 @@
+#' Get theta from chord length
+#'
+#' Returns theta in radians from a chord length in kilometres
+#'
+#' @param chord_length The chord length in kilometres.
+#' @param EarthRad Radius of the Earth in kilometres.
+#' @return Theta in radians.
+#' @details Nothing yet.
+#' @examples
+#' ThetaFromChordLength(1000)
+
 # Get theta in radians from chord length and radius in km:
 ThetaFromChordLength <- function(chord_length, EarthRad = 6367.4447) {
 	
