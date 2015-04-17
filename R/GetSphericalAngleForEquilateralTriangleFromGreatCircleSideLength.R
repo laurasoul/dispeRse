a836bcd2a5a563e590d9a0690a4e9401b1593377
graemetlloyd <graemetlloyd@gmail.com>
2015-04-17 17:18:04 -0400

    Added new internal function.

---
+++ R/GetSphericalAngleForEquilateralTriangleFromGreatCircleSideLength.R
@@ -0,0 +1,18 @@
+# Get the spherical angle for an equilateral triangle where its sides (as great circle distances) are known:
+GetSphericalAngleForEquilateralTriangleFromGreatCircleSideLength <- function(side_length, EarthRad = 6367.4447) {
+	
+# Check this works for triangles larger than equivalent of 90-degrees of latitude on each side!
+	
+	# Get y (which will be double the opposite for the triangle we are trying to solve):
+	y <- GetChordLengthFromTheta(GetThetaFromGreatCircleDistance(side_length))
+	
+	# Get z which will be double the hypotenuse for the triangle we are trying to solve):
+	z <- GetChordLengthFromTheta(GetThetaFromGreatCircleDistance(side_length * 2))
+	
+	# Solve the traingle for theta (the spherical angle) and convert to degrees:
+	spherical_angle <- asin((y / 2) / (z / 2)) / (pi / 180) * 2
+	
+	# Return the spherical angle:
+	return(spherical_angle)
+	
+}
