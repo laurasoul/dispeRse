5bbd283900d02cf02b0652df6037c14d24a8dc5f
graemetlloyd <graemetlloyd@gmail.com>
2015-04-17 18:10:21 -0400

    Added roxygen information.

--- R/GreatCircleDistanceFromTheta.R
+++ R/GreatCircleDistanceFromTheta.R
@@ -1,3 +1,14 @@
+#' Returns great circle distance from theta
+#'
+#' Returns great circle distance in kilometres from theta in radians
+#'
+#' @param theta Theta in radians.
+#' @param EarthRad Radius of the Earth in kilometres.
+#' @return Great circle distance in kilometres.
+#' @details Nothing yet.
+#' @examples
+#' GreatCircleDistanceFromTheta(0.5)
+
 # Get great circle distacne from theta in radians and radius in km:
 GreatCircleDistanceFromTheta <- function(theta, EarthRad = 6367.4447) {
 	
