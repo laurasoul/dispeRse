e54c629ca88485a4968cbf35c52589e780af6cb3
graemetlloyd <graemetlloyd@gmail.com>
2015-04-17 17:16:42 -0400

    Added new internal function.

---
+++ R/GetChordLengthFromTheta.R
@@ -0,0 +1,7 @@
+# Get chord length from theta in radians and radius in km:
+GetChordLengthFromTheta <- function(theta, EarthRad = 6367.4447) {
+	
+	# Get chord length from theta (in radians):
+	sin(theta / 2) * EarthRad * 2
+	
+}
