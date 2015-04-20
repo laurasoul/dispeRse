594515a8872918a040bd2cafc811c922f79718c9
graemetlloyd <graemetlloyd@gmail.com>
2015-04-20 12:30:16 -0400

    Added new conditional for identical points as input.

--- R/GreatCircleDistanceFromLongLat.R
+++ R/GreatCircleDistanceFromLongLat.R
@@ -16,21 +16,35 @@
 # GreatCircleDistance function:
 GreatCircleDistanceFromLongLat <- function(long1, lat1, long2, lat2, EarthRad = 6367.4447) {
 	
-	# Convert point 1 longitude to radians:
-	long1 <- long1 * (pi / 180)
+	# If latitude and longitude describe different points:
+	if(all.equal(long1, long2) != TRUE || all.equal(lat1, lat2) != TRUE) {
 	
-	# Convert point 1 latitude to radians:
-	lat1 <- lat1 * (pi / 180)
-	
-	# Convert point 2 longitude to radians:
-	long2 <- long2 * (pi / 180)
-	
-	# Convert point 2 latitude to radians:
-	lat2 <- lat2 * (pi / 180)
-	
-	# Get great circle distacne using cosine:
-	great_circle_distance <- acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(abs(long1 - long2))) * EarthRad
-	
+		# Convert point 1 longitude to radians:
+		long1 <- long1 * (pi / 180)
+		
+		# Convert point 1 latitude to radians:
+		lat1 <- lat1 * (pi / 180)
+		
+		# Convert point 2 longitude to radians:
+		long2 <- long2 * (pi / 180)
+		
+		# Convert point 2 latitude to radians:
+		lat2 <- lat2 * (pi / 180)
+		
+		# Get great circle distacne using cosine:
+		great_circle_distance <- acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(abs(long1 - long2))) * EarthRad
+		
+	# If latitude and longitude describe the same point (within floating point error):
+	} else {
+		
+		# Warn user that they may have accidentally submitted identical points:
+		print("WARNING: points are identical within floating point error.")
+		
+		# Set distacne to zero:
+		great_circle_distance <- 0
+		
+	}
+		
 	# Return great circle distance:
 	return(great_circle_distance)
 	
