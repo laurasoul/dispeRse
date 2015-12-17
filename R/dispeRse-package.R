08a41d8ae920226abfc03edc23a39fdfd250c9c5
graemetlloyd <graemetlloyd@gmail.com>
2015-12-17 14:34:59 +0000

    Added file to generate NAMESPACE from.

---
+++ R/dispeRse-package.R
@@ -0,0 +1,25 @@
+# devtools::build_win(version = "R-devel")
+
+#' Models For Paleobiogeography
+#' 
+#' Functions for simulating and modelling phylogenetic patterns of paleobiogeography.
+#' 
+#' @name dispeRse-package
+#'
+#' @aliases dispeRse
+#'
+#' @docType package
+#'
+#' @author Graeme T. Lloyd <graemetlloyd@@gmail.com>
+#'
+#' @references Nothing yet.
+#'
+#' @keywords biogeography,phylogeny
+#'
+#' @examples
+#' 
+#' # Nothing yet.
+#'
+#' @import ape
+#' @import Claddis
+#' @import geiger
